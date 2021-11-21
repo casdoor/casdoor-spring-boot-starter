566c911cbf531ee39c2c98d944feb0000434e8d7
Yixiang Zhao <seriouszyx@foxmail.com>
2021-11-21 13:22:11 +0800

    feat: upgrade casdoor-java-sdk and add getCasdoorSmsService, getCasdoorEmailService, getCasdoorResourceService APIs
    Signed-off-by: Yixiang Zhao <seriouszyx@foxmail.com>

--- src/main/java/org/casbin/casdoor/config/CasdoorAutoConfigure.java
+++ src/main/java/org/casbin/casdoor/config/CasdoorAutoConfigure.java
@@ -1,7 +1,6 @@
 package org.casbin.casdoor.config;
 
-import org.casbin.casdoor.service.CasdoorAuthService;
-import org.casbin.casdoor.service.CasdoorUserService;
+import org.casbin.casdoor.service.*;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -16,13 +15,31 @@ public class CasdoorAutoConfigure {
 
     @Bean
     @ConditionalOnMissingBean
-    CasdoorUserService getCasdoorUserService (CasdoorConfiguration config) {
+    CasdoorUserService getCasdoorUserService(CasdoorConfiguration config) {
         return new CasdoorUserService(config);
     }
 
     @Bean
     @ConditionalOnMissingBean
-    CasdoorAuthService getCasdoorAuthService (CasdoorConfiguration config) {
+    CasdoorAuthService getCasdoorAuthService(CasdoorConfiguration config) {
         return new CasdoorAuthService(config);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    CasdoorSmsService getCasdoorSmsService(CasdoorConfiguration config) {
+        return new CasdoorSmsService(config);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    CasdoorEmailService getCasdoorEmailService(CasdoorConfiguration config) {
+        return new CasdoorEmailService(config);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    CasdoorResourceService getCasdoorResourceService(CasdoorConfiguration config) {
+        return new CasdoorResourceService(config);
     }
 }
