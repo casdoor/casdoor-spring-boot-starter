60c1e2a1da9e53ae46e3b754d044f6bca2a125cb
zeekrs <git@zeekrs.dev>
2023-07-28 23:49:55 +0800

    feat: upgrade casdoor-java-sdk to v1.16.0 & add several getXxxServices (#33)
    * feat: upgrade new version for casdoor-java-sdk & add mission services
    
    * Update pom.xml
    
    ---------
    
    Co-authored-by: hsluoyz <hsluoyz@qq.com>

--- src/main/java/org/casbin/casdoor/config/CasdoorAutoConfigure.java
+++ src/main/java/org/casbin/casdoor/config/CasdoorAutoConfigure.java
@@ -42,4 +42,36 @@ public class CasdoorAutoConfigure {
     CasdoorResourceService getCasdoorResourceService(CasdoorConfiguration config) {
         return new CasdoorResourceService(config);
     }
+
+    @Bean
+    @ConditionalOnMissingBean
+    CasdoorAccountService getCasdoorAccountService(CasdoorConfiguration config) {
+        return new CasdoorAccountService(config);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    CasdoorEnforcerService getCasdoorEnforcerService(CasdoorConfiguration config) {
+        return new CasdoorEnforcerService(config);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    CasdoorPermissionService getCasdoorPermissionService(CasdoorConfiguration config) {
+        return new CasdoorPermissionService(config);
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    CasdoorRoleService getCasdoorRoleService(CasdoorConfiguration config) {
+        return new CasdoorRoleService(config);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    CasdoorTokenService getCasdoorTokenService(CasdoorConfiguration config) {
+        return new CasdoorTokenService(config);
+    }
+
 }
